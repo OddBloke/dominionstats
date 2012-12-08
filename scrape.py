e40b6db96e480fdef24fef5ce80d2c6be4daa3bc
Daniel Watkins <daniel@daniel-watkins.co.uk>
2012-12-08 10:48:38 +0000

    Use if __name__ == '__main__' in scrape.py.

--- scrape.py
+++ scrape.py
@@ -8,13 +8,8 @@ import urllib
 
 import utils
 
-parser = utils.incremental_date_range_cmd_line_parser()
-
 # if the size of the game log is less than this assume we got an error page
 SMALL_FILE_SIZE = 5000 
-
-utils.ensure_exists('static/scrape_data')
-os.chdir('static/scrape_data')
 
 # make I should just adopt the isotropic format for consistency?
 ISOTROPIC_FORMAT =   '%(year)d%(month)02d/%(day)02d/all.tar.bz2'
@@ -46,22 +41,27 @@ class MyURLOpener(urllib.FancyURLopener):
         urllib.URLopener.http_error_default(self, *args, **kwargs)
 
 
-args = parser.parse_args()
+if __name__ == '__main__':
+    parser = utils.incremental_date_range_cmd_line_parser()
+    args = parser.parse_args()
 
-for cur_date in utils.daterange(datetime.date(2010, 10, 15), 
-                                datetime.date.today()):
-    str_date = time.strftime("%Y%m%d", cur_date.timetuple())
-    if not utils.includes_day(args, str_date):
-        print 'skipping', str_date, 'because not in cmd line arg daterange'
-        continue
-    directory = str_date
-    print str_date
-    games_short_name = str_date + '.all.tar.bz2'
-    saved_games_bundle = directory + '/' + games_short_name
-    if utils.at_least_as_big_as(saved_games_bundle, SMALL_FILE_SIZE):
-        print 'skipping because exists', str_date, saved_games_bundle, \
-            'and not small (size=', os.stat(saved_games_bundle).st_size, ')'
-    else:
+    utils.ensure_exists('static/scrape_data')
+    os.chdir('static/scrape_data')
+
+    for cur_date in utils.daterange(datetime.date(2010, 10, 15),
+                                    datetime.date.today()):
+        str_date = time.strftime("%Y%m%d", cur_date.timetuple())
+        if not utils.includes_day(args, str_date):
+            print 'skipping', str_date, 'because not in cmd line arg daterange'
+            continue
+        directory = str_date
+        print str_date
+        games_short_name = str_date + '.all.tar.bz2'
+        saved_games_bundle = directory + '/' + games_short_name
+        if utils.at_least_as_big_as(saved_games_bundle, SMALL_FILE_SIZE):
+            print 'skipping because exists', str_date, saved_games_bundle, \
+                'and not small (size=', os.stat(saved_games_bundle).st_size, ')'
+            continue
         if not os.path.exists(directory):
             os.mkdir(directory)
         RemoveSmallFileIfExists(saved_games_bundle)
